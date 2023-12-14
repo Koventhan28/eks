a43cc457c12840eda27751a1dae8f99fc015bf8e
Koventhan <k.koventhan@gmail.com>
2023-12-14 15:24:09 +0530

    eks creation file

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+  assume_role_policy = <<EOF
+  {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
